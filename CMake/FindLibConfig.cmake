fbc2e8614fee8cfd5c91c82dfea0e38dca010481
V V <vvaltman@aurum>
2016-04-22 19:56:16 +0300

    cmake: fixed style of findlibconfig

--- CMake/FindLibConfig.cmake
+++ CMake/FindLibConfig.cmake
@@ -1,7 +1,10 @@
-find_path(LIBCONFIG_INCLUDE_DIR libconfig.h)
+if (LIBCONFIG_INCLUDE_DIR AND LIBCONFIG_LIBRARY)
+  set (LIBCONFIG_FOUND TRUE)
+else (LIBCONFIG_INCLUDE_DIR AND LIBCONFIG_LIBRARY)
+  find_path(LIBCONFIG_INCLUDE_DIR libconfig.h)
+  find_library(LIBCONFIG_LIBRARY config)
 
-find_library(LIBCONFIG_LIBRARY config)
+  include(FindPackageHandleStandardArgs)
 
-include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(LibConfig REQUIRED_VARS LIBCONFIG_INCLUDE_DIR LIBCONFIG_LIBRARY)
+  find_package_handle_standard_args(LibConfig REQUIRED_VARS LIBCONFIG_INCLUDE_DIR LIBCONFIG_LIBRARY)
+endif (LIBCONFIG_INCLUDE_DIR AND LIBCONFIG_LIBRARY)
